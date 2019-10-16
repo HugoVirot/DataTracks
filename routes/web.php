0b71341e14b071248895068ce2b07a79cc48e88b
Hugo Virot <hugo.virot@gmail.com>
2019-10-16 10:39:33 +0200

    version de base crud Campagnes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/campagne', 'HomeController@index')->name('index');
+//Route::get('/home', 'HomeController@home')->name('home');
+
+Auth::routes();                                                             //ensemble des routes de l'authentification
+
+//Route::get('/quacks/search', 'QuackController@search')->name('quacks.search');
+//Route::resource('/quacks', 'QuackController')->except('index');
+//
+//
+//Route::resource('/comments', 'CommentController')->except('index');
+//
+//Route::get('users/{user}', 'User\UserController@profil')->name('user.profil');
+//
+//Route::get('settings/account', 'User\UserController@index')->name('user.account');
+//Route::get('settings/account/edit', 'User\UserController@edit')->name('user.account.edit');
+//Route::put('settings/account/update', 'User\UserController@update')->name('user.account.update');
